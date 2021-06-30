79d2a30f1abe5139ebe6615be1e1c11647ada82a
Santiago Feijoo <santiago_feijoo@outlook.com>
2021-06-30 16:51:27 -0500

    primer avance

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+  pantalla:number = 0;
+  resultado:string = "";
+  constructor() { }
+
+  onValor(numero:number): void {
+    this.resultado = this.resultado + numero
+
+  }
+
+  onSumar(): void {
+      this.resultado = this.resultado + this.pantalla
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
